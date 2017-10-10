bd7daabff40f859956616681c665dd0bf3f5ea78
T162880 <T162880@MMSURMAWUW8.mmi.mig.corp>
2017-10-10 13:15:30 -0400

    amazon claims

--- openw.py
+++ openw.py
@@ -1,2 +1,2 @@
 print("hello")
-https://www.amazon.com/clouddrive/share/tarizGTfPMFciocn1e82oSTtVkF1qCbtvmtvRjIYJW6
+amazon_claim_data = "https://www.amazon.com/clouddrive/share/tarizGTfPMFciocn1e82oSTtVkF1qCbtvmtvRjIYJW6"
